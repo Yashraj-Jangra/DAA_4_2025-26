cd150581d08d7b9b1addde4b8ecd693ac5d97a20
Yashraj <84060578+Yashraj-Jangra@users.noreply.github.com>
2026-01-16 11:23:46 +0530

    added exp 1

---
+++ exp1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+void complexRec(int n) {
+
+
+   if (n <= 2) {
+       return;
+   }
+
+
+   int p = n;
+   while (p > 0) {
+       vector<int> temp(n);
+       for (int i = 0; i < n; i++) {
+           temp[i] = i ^ p;
+       }
+       p >>= 1;
+   }
+
+
+   vector<int> small(n);
+   for (int i = 0; i < n; i++) {
+       small[i] = i * i;
+   }
+
+
+   if (n % 3 == 0) {
+       reverse(small.begin(), small.end());
+   } else {
+       reverse(small.begin(), small.end());
+   }
+
+
+   complexRec(n / 2);
+   complexRec(n / 2);
+   complexRec(n / 2);
+}
+int main() {
+    int n = 10; // Example input size
+    for(int i=0; i<n;){
+        printf(complexRec(i));
+    }
+    return 0;
+    }
